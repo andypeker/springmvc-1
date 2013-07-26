9f899743578cc450ad49a1b082bd40ddb138e1d6
zhaiyz <hanxue_lang@foxmail.com>
2013-07-26 17:43:26 +0800

    添加支持以xml,json形式返回内容的功能

--- src/main/java/com/zhaiyz/controller/HelloWorldController.java
+++ src/main/java/com/zhaiyz/controller/HelloWorldController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.zhaiyz.vo.User;
+
 @Controller
 public class HelloWorldController {
 	
@@ -33,6 +35,11 @@ public class HelloWorldController {
 	@RequestMapping("/fuck")
 	public ModelAndView sayFuck(String message) {
 		return new ModelAndView("hello", "message", message);
+	}
+	
+	@RequestMapping("/user")
+	public ModelAndView printUser(User user) {
+		return new ModelAndView("hello", "message", user);
 	}
 
 }
